919d1e0b1715093730c855772653876d0bad4612
Luka <luka@Lukas-MacBook-Pro.local>
2026-03-15 18:32:21 +0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { AppPreloader } from "@/components/app-preloader";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +12,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ka-GE">
       <body>
-        <AppPreloader />
         <main className="min-h-screen w-full bg-background px-4 py-4 sm:px-6 lg:px-8">
           {children}
         </main>
